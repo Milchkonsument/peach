5ac61017dd5daca92e8311737ea0e35bd4572784
Milchkonsument <m.domroese@neox-studios.de>
2022-08-03 15:36:19 +0200

    - added file file reading and screen clear to repl mode

--- interpreter/main.cs
+++ interpreter/main.cs
@@ -3,12 +3,17 @@ namespace PeachInterpreter
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
 
     class Interpreter
     {
-        static Dictionary<String, Tuple<String, Action>> commands = new Dictionary<string, Tuple<string, Action>>() {
+        static List<string> cmd_args = new List<string>();
+
+        static readonly Dictionary<String, Tuple<String, Action>> commands = new Dictionary<string, Tuple<string, Action>>() {
             {"q", new Tuple<String, Action>("quits the session", command_quit)},
             {"h", new Tuple<String, Action>("prints help", command_help)},
+            {"c", new Tuple<String, Action>("clear screen", command_clear)},
+            {"r", new Tuple<String, Action>("run file (usage: :r *.peach)", run_file)},
         };
 
         static void Main(string[] args)
@@ -19,10 +24,7 @@ namespace PeachInterpreter
             }
             else
             {
-                if (File.Exists(args[1]))
-                {
-                    // blah
-                }
+                run_file();
             }
         }
 
@@ -41,14 +43,39 @@ namespace PeachInterpreter
 
                 if (ln.StartsWith(':'))
                 {
-                    String cmd = ln.Substring(1);
-                    if (commands.ContainsKey(cmd))
-                        commands[cmd].Item2();
+                    String cmd_full = ln.Substring(1);
+                    String[] cmd = cmd_full.Split(' ');
+                    if (cmd.Length > 1)
+                        cmd_args = cmd.ToList().GetRange(1, cmd.Length - 1);
+                    if (commands.ContainsKey(cmd[0]))
+                        commands[cmd[0]].Item2();
                     else
-                        Console.WriteLine($"Command '{cmd}' does not exist.");
+                        Console.WriteLine($"Command '{cmd_full[0]}' does not exist.");
                 }
                 else
                     eval(ln);
+            }
+        }
+
+        static void run_file()
+        {
+            if (cmd_args.Count == 0)
+            {
+                Console.WriteLine("Please provide a full file path to *.peach file");
+                return;
+            }
+
+            if (!File.Exists(cmd_args[0]))
+            {
+
+                Console.WriteLine("File does not exist.");
+                return;
+            }
+
+            if (cmd_args[0].EndsWith(".peach"))
+            {
+                Console.WriteLine("Please provide a *.peach file.");
+                return;
             }
         }
 
@@ -59,7 +86,15 @@ namespace PeachInterpreter
 
         static void command_help()
         {
-            Console.WriteLine("Nobody is here to help. :<");
+            foreach (KeyValuePair<String, Tuple<String, Action>> item in commands)
+            {
+                Console.WriteLine($":{item.Key} | {item.Value.Item1}");
+            }
+        }
+
+        static void command_clear()
+        {
+            Console.Write("\u001b[2J\u001b[H");
         }
     }
 }
